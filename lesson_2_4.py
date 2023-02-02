1d79fdbe28449a62f6acd53977b89cd9d212d73f
Mits16 <mits1388@gmail.com>
2023-02-02 19:24:38 +0300

    update lesson_2_4

--- lesson_2_4.py
+++ lesson_2_4.py
@@ -1,29 +1,15 @@
 #Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
-first_number = int(input('Введите первое число: '))
-second_number = int(input('Введите второе число: '))
-third_number = int(input('Введите третье число: '))
-
+cycle_numbers = 0
 positive_number = 0
 negative_number = 0
 
-if first_number >= 0:
-    positive_number += 1
-else:
-    negative_number += 1
-if second_number >= 0:
-    positive_number += 1
-else:
-    negative_number += 1
-if third_number >= 0:
-    positive_number += 1
-else:
-    negative_number += 1
+while cycle_numbers < 3:
+    number = int(input(f'Enter {cycle_numbers + 1} number: '))
+    if number >= 0:
+        positive_number += 1
+    else:
+        negative_number += 1
+    cycle_numbers += 1
 
-if positive_number == 1:
-    print(f'Пользователь ввел {positive_number} положительное число')
-else:
-    print(f'Пользователь ввел {positive_number} положительных числа')
-if negative_number == 1:
-    print(f'Пользователь ввел {negative_number} отрицательное число')
-else:
-    print(f'Пользователь ввел {negative_number} отрицательных числа')
+print(f'User enter {positive_number} positive number')
+print(f'User enter {negative_number} negative number')
