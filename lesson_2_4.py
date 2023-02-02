80542625b5aea06b9b6c33a5ad8ab62475b2e75c
Mits16 <mits1388@gmail.com>
2023-02-02 21:37:15 +0300

    corrected lesson_2_4

--- lesson_2_4.py
+++ lesson_2_4.py
@@ -1,15 +1,7 @@
-#Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
-cycle_numbers = 0
-positive_number = 0
-negative_number = 0
+# Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+number_first = int(input('Enter the number one: '))
+number_second = int(input('Enter the number two: '))
+number_third = int(input('Enter the number three: '))
+print('User enter positive number', (number_first >= 0) + (number_second >= 0) + (number_third >= 0))
+print('User enter negative number', (number_first < 0) + (number_second < 0) + (number_third < 0))
 
-while cycle_numbers < 3:
-    number = int(input(f'Enter {cycle_numbers + 1} number: '))
-    if number >= 0:
-        positive_number += 1
-    else:
-        negative_number += 1
-    cycle_numbers += 1
-
-print(f'User enter {positive_number} positive number')
-print(f'User enter {negative_number} negative number')
