7f7da13142c41c692b5640631c3e7280c9fa5c32
Mits16 <mits1388@gmail.com>
2023-02-03 19:53:00 +0300

    update lesson_2_6

--- lesson_2_7.py
+++ lesson_2_7.py
@@ -1,7 +1,4 @@
 # Вводится слово, вывести True если слово является палиндромом (читается справа налево как слева направо), False в противном случае
 word = input('Enter word: ')
-if word[:] == word[::-1]:
-    print('True')
-else:
-    print('False')
+print(word[:] == word[::-1])
 
