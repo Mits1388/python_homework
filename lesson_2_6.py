54c39d928064ee0e29ae56ea20c90b4cc6ba17f2
Mits16 <mits1388@gmail.com>
2023-02-03 19:45:40 +0300

    update lesson_2_6

--- lesson_2_6.py
+++ lesson_2_6.py
@@ -2,12 +2,11 @@
 # состоящая минимум из 3х слов, с помощью методов поиска, срезов и конкатенации,
 # необходимо поменять местами первое и последнее слово в строке
 text = input('Enter three words: ')
-t = text.find(' ')
-e = text.rfind(' ')+1
-first_word = text[:t]
-second_word = text[e:]
-third_word = text[t:e]
+end_position = text.find(' ')
+# print(end_position)
+start_position = text.rfind(' ') + 1
+# print(start_position)
+first_word = text[:end_position]
+second_word = text[start_position:]
+third_word = text[end_position:start_position]
 print(f'{second_word}{third_word}{first_word}')
-
-
-
