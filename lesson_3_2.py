ab9b2647d89c9a78fb4f68fa624722e40e6feb04
Mits16 <mits1388@gmail.com>
2023-02-07 20:10:43 +0300

    fix lesson_3_2

--- lesson_3_2.py
+++ lesson_3_2.py
@@ -1,7 +1,6 @@
 # Без использования collections, написать программу, которая будет
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
-
 text = input('Enter string: ')
 number_of_letters = {i: text.count(i) for i in text}
 print(number_of_letters)
