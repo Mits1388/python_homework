0a5d78bd0b4b3d6b87b40989e12b097934ad646a
Mits16 <mits1388@gmail.com>
2023-02-08 19:59:33 +0300

    fix lesson_3_3

--- lesson_3_3.py
+++ lesson_3_3.py
@@ -3,8 +3,8 @@
 # для этих ключей будут браться с клавиатуры
 from pprint import pprint
 
-numbers = int(input('Enter number: '))
-users = {k: {'name': input(f'name {k+1}: '), 'email': input(f'email {k+1}: ')} for k in range(numbers)}
+number = int(input('Enter number: '))
+users = {k: {'name': input(f'name {k+1}: '), 'email': input(f'email {k+1}: ')} for k in range(number)}
 pprint(users)
 
 # user = {
