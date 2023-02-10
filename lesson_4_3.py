ad69d3f0774031168b28cd2d861da015c370044f
Mits16 <mits1388@gmail.com>
2023-02-10 17:47:30 +0300

    add lesson_4_3.py

--- lesson_4_3.py
+++ lesson_4_3.py
@@ -1,9 +1,9 @@
-#Вывести четные числа от 2 до N по 5 в строку
+# Вывести четные числа от 2 до N по 5 в строку
 n = int(input('Enter number: '))
 numbers = []
 for i in range(1, n + 1):
     if i % 2 == 0:
         numbers.append(i)
-#print(numbers)
+# print(numbers)
 for i in range(0, len(numbers), 5):
     print(numbers[i:i + 5])
