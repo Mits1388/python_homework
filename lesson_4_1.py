503a7de1bd70a59244f407baad62aa2310fa086d
Mits16 <mits1388@gmail.com>
2023-02-10 17:46:38 +0300

    add lesson_4_1.py

--- lesson_4_1.py
+++ lesson_4_1.py
@@ -1,10 +1,5 @@
 # Вывести первые N цисел кратные M и больше K
 
-# m = int(input('Enter M: '))
-# k = int(input('Enter K: '))
-# for i in range(1, k + 1):
-#     if i % m == 0:
-#         print(i)
 n = int(input('Enter N: '))
 m = int(input('Enter M: '))
 k = int(input('Enter K: '))
