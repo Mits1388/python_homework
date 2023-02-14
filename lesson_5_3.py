1fdb19b4f923f009c5e4ef2e499a7aa1542f2590
Mits16 <mits1388@gmail.com>
2023-02-14 21:21:09 +0300

    add lesson_5_3.py

---
+++ lesson_5_3.py
@@ -0,0 +1,11 @@
+# Дан список чисел и на вход поступает число N, необходимо сместить список на
+# указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
+import random
+
+n = int(input('Enter N: '))
+list_random = [random.randint(0, 100) for i in range(0, 10)]
+print(list_random)
+
+list_shift = list_random[n:]+list_random[:n]
+print(list_shift)
+
