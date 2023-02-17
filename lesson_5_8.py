78d5eb16a69fbd753c3479e151585cf425c9edda
Mits16 <mits1388@gmail.com>
2023-02-17 19:14:23 +0300

    fix lesson_5_8.py

--- lesson_5_8.py
+++ lesson_5_8.py
@@ -1,16 +1,16 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
 
-d = {
-    'Belarus': {'Minsk', 'Grodno', 'Molodechno'},
-    'Russia': {'Moscow', 'Saint Petersburg', 'Omsk'},
-    'Ukraine': {'Kyiv', 'Odessa', 'Lviv'},
-    'Georgia': {'Tbilisi', 'Batumi', 'Kutaisi'}
+dictionary = {
+    'Belarus': ('Minsk', 'Grodno', 'Molodechno'),
+    'Russia': ('Moscow', 'Saint Petersburg', 'Omsk'),
+    'Ukraine': ('Kyiv', 'Odessa', 'Lviv'),
+    'Georgia': ('Tbilisi', 'Batumi', 'Kutaisi')
 }
 
 name_city = input('Enter city: ')
 
-for i, j in d.items():
-    for k in d.get(i):
+for i, j in dictionary.items():
+    for k in dictionary.get(i):
         if k == name_city:
             print(f'This city is in {i}')
