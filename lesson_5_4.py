8b5acd4cf34f3de9b6df65f254197b9397ded411
Mits16 <mits1388@gmail.com>
2023-02-19 14:21:01 +0300

    fix lesson_5_4.py

--- lesson_5_4.py
+++ lesson_5_4.py
@@ -3,15 +3,15 @@
 # незаконно
 
 
-list_different_types = [77, 'hello', 45, 54, 'world', 4.4, None]
+list_types = [77, 'hello', 45, 54, 'world', 4.4, None]
 
 
-def is_type(list_any_types):
+def is_type(any_types):
     a = []
-    for i in list_any_types:
+    for i in any_types:
         if isinstance(i, str):
             a.append(i)
     return a
 
 
-print(is_type(list_different_types))
+print(is_type(list_types))
