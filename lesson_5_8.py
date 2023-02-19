f0f8c61617d8eb3636bc8315f0325b67af58521d
Mits16 <mits1388@gmail.com>
2023-02-19 16:01:13 +0300

    fix lesson_5_8.py

--- lesson_5_8.py
+++ lesson_5_8.py
@@ -1,16 +1,32 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
+import dictionary as dictionary
 
 dictionary = {
-    'Belarus': ('Minsk', 'Grodno', 'Molodechno'),
-    'Russia': ('Moscow', 'Saint Petersburg', 'Omsk'),
-    'Ukraine': ('Kyiv', 'Odessa', 'Lviv'),
-    'Georgia': ('Tbilisi', 'Batumi', 'Kutaisi')
+    'Belarus': {'Minsk', 'Grodno', 'Molodechno'},
+    'Russia': {'Moscow', 'Saint Petersburg', 'Omsk'},
+    'Ukraine': {'Kyiv', 'Odessa', 'Lviv'},
+    'Georgia': {'Tbilisi', 'Batumi', 'Kutaisi'}
 }
 
 name_city = input('Enter city: ')
 
-for i, j in dictionary.items():
-    for k in dictionary.get(i):
-        if k == name_city:
-            print(f'This city is in {i}')
+
+def is_country(any_dictionary):
+    for i, j in dictionary.items():
+        if name_city in j:
+            return i
+
+
+print(f'This city is in {is_country(dictionary)}')
+
+#
+# for i, j in dictionary.items():
+#     for k in dictionary.get(i):
+#         if k == name_city:
+#             print(f'This city is in {i}')
+#
+#             key = 'email'
+#             for i, j in dictionary.items():
+#                 if key not in j or j.get(key) == '':
+#                     print(j.get('first name') + ' doesn`t have an email')
