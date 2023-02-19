d4091b9b35463e4d36dac63620baa323cb9dc8a3
Mits16 <mits1388@gmail.com>
2023-02-19 14:48:48 +0300

    fix lesson_5_1.py

--- lesson_5_1.py
+++ lesson_5_1.py
@@ -2,16 +2,28 @@
 # использования функции int
 
 decimal_number = int(input('Enter decimal number: '))
-b = ''
-while decimal_number > 0:
-    b = str(decimal_number % 2) + b
-    decimal_number //= 2
-print(f'Binary number = {b}')
+
+
+def is_convert_bin(number):
+    b = ''
+    while number > 0:
+        b = str(number % 2) + b
+        number //= 2
+    return b
+
+
+print(f'Binary number = {is_convert_bin(decimal_number)}')
 
 binary_number = input('Enter binary number: ')
-a = []
-n = 1
-for i in reversed(binary_number):
-    a.append(int(i)*n)
-    n *= 2
-print(f'Decimal number: {sum(a)}')
+
+
+def is_convert_dec(number):
+    a = []
+    n = 1
+    for i in reversed(number):
+        a.append(int(i) * n)
+        n *= 2
+    return sum(a)
+
+
+print(f'Decimal number: {is_convert_dec(binary_number)}')
