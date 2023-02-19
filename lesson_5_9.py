4ce5677383d301bc24d51f7cd16d4a75a690c10c
Mits16 <mits1388@gmail.com>
2023-02-19 20:13:07 +0300

    fix lesson_5_9.py

--- lesson_5_9.py
+++ lesson_5_9.py
@@ -9,14 +9,20 @@ dictionary = {
     2: {'first name': 'Roy', 'last name': 'Jones', 'phone': '1 340 456 8546', 'email': ''},
     3: {'first name': 'Philip', 'last name': 'Lum', 'phone': '1 671 333 1451'},
     4: {'first name': 'Jessica', 'last name': 'Parker', 'phone': '1 721 777 7777', 'email': 'jessica@gmail.com'},
-    5: {'first name': 'Marie', 'last name': 'Broadbet', 'phone': '53 343 45 342', 'email': 'marie@gmail.com'},
+    5: {'first name': 'Marie', 'last name': 'Broadbet', 'phone': '53 343 45 342', 'email': 'marie@gmail.com'}
 }
-
 key = 'email'
-for i, j in dictionary.items():
-    if key not in j or j.get(key) == '':
-        print(j.get('first name') + ' doesn`t have an email')
 
+
+def no_email(any_dictionary, any_key):
+    string = ''
+    for i, j in any_dictionary.items():
+        if any_key not in j or j.get(any_key) == '':
+            string += j.get('first name') + ' '
+    return string
+
+
+print(f'{no_email(dictionary, key)}don`t have an email')
 # key = 'email'
 # for i, j in dictionary.items():
 #     for k, v in j.items():
@@ -25,4 +31,3 @@ for i, j in dictionary.items():
 #                 print(j.get('first name') + ' doesn`t have an email')
 #     if key not in j:
 #         print(j.get('first name') + ' doesn`t have an email')
-
