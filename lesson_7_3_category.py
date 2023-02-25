cf7e4a8ee556f3c9d1ffc26b36acd55edf221c70
Mits16 <mits1388@gmail.com>
2023-02-25 18:50:37 +0300

    fix lesson_7_3_category.py

---
+++ lesson_7_3_category.py
@@ -0,0 +1,25 @@
+class Category:
+    categories: str
+
+
+    def __init__(self, categories):
+        self.categories = categories
+
+    def add_categories():
+        if category == None:
+
+
+        else:
+            try:
+            except ValueError as e:
+                print('ValueError exception')
+
+
+    def get_categories(self):
+
+
+    def delete_categories(self):
+
+    def update(self):
+
+
