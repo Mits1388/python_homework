033050ae66a8f892f56ce5cb036ba1822121c71c
Mits16 <mits1388@gmail.com>
2023-02-25 18:09:02 +0300

    fix lesson_7_2_taxi.py

--- lesson_7_2_taxi.py
+++ lesson_7_2_taxi.py
@@ -1,9 +1,4 @@
 # 2. Написать класс Taxi Конструктор класса принимает атрибуты: cars: list[Car] (список экземпляров класса Car)
-# 2.1 Реализовать метод find_car На вход метода поступают атрибуты: count_passengers, is_baby (количество пассажиров,
-# наличие ребенка, примечание: количество пассажиров с учетом ребенка если он есть)На основании данных,
-# вернуть объект Car из атрибута cars подходящий по параметрам исвободный (is_busy = False),
-# у автомобиля сменить атрибут is_busy на значение True, если подходящего автомобиля нет, метод должен возвращать None
-
 class Car:
     color: str
     count_passenger_seats: int
@@ -16,31 +11,33 @@ class Car:
         self.is_baby_seat = is_baby_seat
         self.is_busy = is_busy
 
-    def __str__(self):
-        return f'Car {self.color = }; {self.count_passenger_seats = }; ' \
-               f'{self.is_baby_seat = }; {self.is_busy = }'
+
+bmw = Car('red', 2, True, False)
+audi = Car('black', 4, False, False)
+volvo = Car('white', 4, False, False)
+mazda = Car('blue', 4, True, False)
+
+cars = [bmw, audi, volvo, mazda]
 
 
 class Taxi:
 
     def __init__(self):
-        self.cars = []
+        # print('init')
+        self.cars = cars
 
-    def __str__(self):
-        i = 0
-        while i != len(taxi.cars):
-            print(f'{self.cars[i]}')
-            i += 1
+# 2.1 Реализовать метод find_car На вход метода поступают атрибуты: count_passengers, is_baby (количество пассажиров,
+# наличие ребенка, примечание: количество пассажиров с учетом ребенка если он есть)На основании данных,
+# вернуть объект Car из атрибута cars подходящий по параметрам исвободный (is_busy = False),
+# у автомобиля сменить атрибут is_busy на значение True, если подходящего автомобиля нет, метод должен возвращать None
+    def find_car(self, count_passengers, is_baby):
+        # print('find_car')
+        for i in self.cars:
+            if count_passengers + is_baby <= i.count_passenger_seats and i.is_baby_seat:
+                if not i.is_busy:
+                    return i.color, i.count_passenger_seats, i.is_baby_seat, i.is_busy
+                i.is_busy = True
 
-    def add_car(self, car):
-        self.cars.append(car)
-
-
-bmw = Car('red', 2, True, True)
-mazda = Car('white', 4, False, False)
 
 taxi = Taxi()
-taxi.add_car(bmw)
-taxi.add_car(mazda)
-
-taxi.__str__()
+print(taxi.find_car(2, True))
