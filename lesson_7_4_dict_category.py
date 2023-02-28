a7457b8d6c26f4d3d74e751de2b1e97b7bfd4262
Mits16 <mits1388@gmail.com>
2023-02-28 20:16:39 +0300

    fix lesson_7_4_dict_category.py

--- lesson_7_4_dict_category.py
+++ lesson_7_4_dict_category.py
@@ -1,9 +1,10 @@
 # 4. Изменить класс Category из задачи 3, список категорий должен содержать не просто имена категорий, а
 # словари с данными о каждой категории (name: str, is_published: bool), а так же изменить
 # методы add, get, delete, update для работы с списком словарей
+
 dict_categories = {
-    0: {'name': 'bus', 'is_published': False},
-    1: {'name': 'sports', 'is_published': False},
+    0: {'name': 'bus', 'is_published': True},
+    1: {'name': 'sports', 'is_published': True},
     2: {'name': 'electric', 'is_published': True},
     3: {'name': 'truck', 'is_published': False},
     4: {'name': 'ambulance', 'is_published': True}
@@ -11,65 +12,65 @@ dict_categories = {
 
 
 class Category:
-    def __init__(self):
-        # print('init')
-        self.categories = dict_categories
 
-    def add(self, category_car):
-        # print('add_categories')
-        for i in self.categories:
-            if i == category_car:
+    def __init__(self):
+        self.dict_categories = dict_categories
+
+    def add(self, category_car, is_published):
+        for i, j in self.dict_categories.items():
+            if category_car == j['name']:
                 raise ValueError('There is a category in the list')
             else:
-                if category_car in self.categories:
+                if i == len(self.dict_categories) - 1:
+                    self.dict_categories[i + 1] = dict(name=category_car, is_published=is_published)
+                    return f'index {len(self.dict_categories.keys()) - 1}'
+                else:
+                    continue
+
+    def get(self, index):
+        for i, j in self.dict_categories.items():
+            if i == index:
+                return j['name']
+        else:
+            raise IndexError('No such index in the list')
+
+    def delete(self, index):
+        for i, j in list(self.dict_categories.items()):
+            if i == index:
+                del self.dict_categories[i]
+
+    def update(self, index, new_category):
+        for i, j in self.dict_categories.items():
+            if new_category == j['name']:
+                raise ValueError('There is a category in the list')
+            else:
+                if new_category in self.dict_categories:
                     continue
                 else:
-                    self.categories.append(category_car)
-                    return f'index {self.categories.index(category_car)}'
+                    self.dict_categories[index] = dict(name=new_category, is_published=False)
+                    return self.dict_categories
+
+    # 4.1 Добавить метод make_published принимающий индекс категории и меняющий значение
+    # ключа is_published на True, если такого индекса нет, вызвать исключение IndexError
+    def make_published(self, index):
+        return
+
+    # 4.2 Добавить метод make_unpublished принимающий индекс категории и меняющий
+    # значение ключа is_published на False, если такого индекса нет, вызвать исключение IndexError
+    def make_unpublished(self, index):
+        return
 
 
+category = Category()
+print(f'Method of adding - {category.add("car", True)}')
+print(f'Method of getting - {category.get(5)}')
+print(f'Before deleting the index - {category.dict_categories}')
+category.delete(3)
+print(f'After deleting the index - {category.dict_categories}')
+print(f'After updating - {category.update(3, "taxi")}')
 
-
-
-#
-#     def get(self, index):
-#         for i in self.categories:
-#             if self.categories.index(i) == index:
-#                 return i
-#         else:
-#             raise IndexError('No such index in the list')
-#
-#     def delete(self, index):
-#         for i in self.categories:
-#             if self.categories.index(i) == index:
-#                 self.categories.remove(i)
-#
-#     def update(self, index, new_category):
-#         for i in self.categories:
-#             if i == new_category:
-#                 raise ValueError('There is a category in the list')
-#             else:
-#                 if new_category in self.categories:
-#                     continue
-#                 else:
-#                     self.categories.insert(index, new_category)
-#                     return self.categories
-#
-#     # 4.1 Добавить метод make_published принимающий индекс категории и меняющий значение ключа is_published на
-#     # True, если такого индекса нет, вызвать исключение IndexError
-#     def make_published(self, index):
-#         return self.categories
-#
-#     # 4.2 Добавить метод make_unpublished принимающий индекс категории и меняющий значение ключа is_published
-#     # на False, если такого индекса нет, вызвать исключение IndexError
-#     def make_unpublished(self, index):
-#         return self.categories
-#
-#
-# category = Category()
-# print(f'Method of adding - {category.add("car")}')
-# print(f'Method of getting - {category.get(5)}')
-# print(f'Before deleting the index - {category.categories}')
-# category.delete(4)
-# print(f'After deleting the index - {category.categories}')
-# print(f'After updating - {category.update(2, "taxi")}')
+# 0: {'name': 'bus', 'is_published': False},
+# 1: {'name': 'sports', 'is_published': False},
+# 2: {'name': 'electric', 'is_published': True},
+# 3: {'name': 'truck', 'is_published': False},
+# 4: {'name': 'ambulance', 'is_published': True}
