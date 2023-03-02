23b44cb49587bcedf60943364bd0f1e57589fbe0
Mits16 <mits1388@gmail.com>
2023-03-02 21:34:42 +0300

    fix lesson_9_sql.py

--- lesson_9_sql.py
+++ lesson_9_sql.py
@@ -2,10 +2,11 @@ import csv
 from database import SessionLocal, engine
 import models
 
-db = SessionLocal()
-
-models.Base.metadata.create_all(bind=engine)
+# db = SessionLocal()
+#
+# models.Base.metadata.create_all(bind=engine)
 
 file = open("bhcorp.csv", "r", encoding='utf-8')
 print(file.read())
-db.close()
+file.close()
+# db.close()
