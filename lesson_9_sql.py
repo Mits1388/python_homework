1be45bc3974f985a6060ee0411a28d0097ab1a60
Mits16 <mits1388@gmail.com>
2023-03-02 21:23:07 +0300

    fix lesson_9_sql.py

--- lesson_9_sql.py
+++ lesson_9_sql.py
@@ -1,29 +1,11 @@
 import csv
-import models
 from database import SessionLocal, engine
+import models
 
 db = SessionLocal()
 
 models.Base.metadata.create_all(bind=engine)
 
-with open("bhcorp.csv", "r") as f:
-    csv_reader = csv.DictReader(f)
-
-    for row in csv_reader:
-        db_record = models.Record(
-            id=row["id"],
-            name=row["name"],
-            user_id=row["user_id"],
-            status_id=row["status_id"],
-            email=row["email"],
-            order_id=row["order_id"],
-            product_id=row["product_id"],
-            title=row["title"],
-            description=row["description"],
-            category_id=row["description"],
-        )
-        db.add(db_record)
-
-    db.commit()
-
+file = open("bhcorp.csv", "r", encoding='utf-8')
+print(file.read())
 db.close()
